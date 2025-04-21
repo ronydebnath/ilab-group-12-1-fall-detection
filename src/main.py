f24a39ea161d654d3a90a0138ee50e9dff430425
Rony Debnath <rony.cse09@gmail.com>
2025-04-22 02:23:21 +1000

    WIP - POC on MNIST Dataset

--- src/main.py
+++ src/main.py
@@ -1,34 +1,19 @@
-# src/main.py
-
 import fedml
 from fedml import FedMLRunner
 
-def run_federated_learning():
-    # 1) parse args from fedml_config.yaml
-    args = fedml.init()
-
-    # 2) select device (CPU/GPU)
-    device = fedml.device.get_device(args)
-
-    # 3) load your Mobifall dataset
-    #    you’ll need to implement data loader under fedml.data.load
-    dataset, output_dim = fedml.data.load(args)
-
-    # 4) create or wrap your model
-    model = fedml.model.create(args, output_dim)
-
-    # 5) start the federated protocol
-    FedMLRunner(args, device, dataset, model).run()
-
-# if __name__ == "__main__":
-#     run_federated_learning()
-
-# if __name__ == "__main__":
-#     fedml.run_simulation()
-
 if __name__ == "__main__":
+    # init FedML framework
     args = fedml.init()
+
+    # init device
     device = fedml.device.get_device(args)
+
+    # load data
     dataset, output_dim = fedml.data.load(args)
+
+    # load model
     model = fedml.model.create(args, output_dim)
-    FedMLRunner(args, device, dataset, model).run()
+
+    # start training
+    fedml_runner = FedMLRunner(args, device, dataset, model)
+    fedml_runner.run()
